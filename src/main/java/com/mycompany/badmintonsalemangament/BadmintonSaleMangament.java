4de45ed8870b09bcface0069383d95ff47d0647f
NguyenToanThang-kotaru <NguyenToanThang15122005@gmail.com>
2025-02-23 10:35:55 +0700

    hello

---
+++ src/main/java/com/mycompany/badmintonsalemangament/BadmintonSaleMangament.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.badmintonsalemangament;
+
+/**
+ *
+ * @author Thang Nguyen
+ */
+public class BadmintonSaleMangament {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
