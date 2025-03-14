89a48116f81f084f51d79624cb6a220980419f49
phattin <tin220905@gmail.com>
2025-03-14 21:34:43 +0700

    NCC

---
+++ ManagmentBadminton/src/main/java/GUI/SupplierGUI.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import BUS.SupplierBUS;
+import DTO.SupplierDTO;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class SupplierGUI {
+    private JPanel panel;
+
+    public SupplierGUI(JPanel panel){
+        this.panel = panel;
+        initComponent();
+    }
+    
+    public void initComponent(){
+    }
+}
