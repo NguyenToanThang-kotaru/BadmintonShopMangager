1da69b2d49f89d4ff22d22b5fd3e3e19a12ce33d
tungthien408 <tungthien408@gmail.com>
2025-04-19 12:40:34 +0700

    ĐÃ xong hóa đơn bán và bán hàng (chưa có serial number)

--- ManagmentBadminton/src/main/java/GUI/GUI_MainLayout.java
+++ ManagmentBadminton/src/main/java/GUI/GUI_MainLayout.java
@@ -17,7 +17,7 @@ public class GUI_MainLayout extends JFrame {
 
     private GUI_Sidebar Sidebar;
     private GUI_TittleBar tittleBar;
-
+    
     public GUI_MainLayout(JFrame login, AccountDTO logined) {
         setTitle("Quản Lý Kho Hàng");
         setSize(1000, 600);
@@ -25,7 +25,7 @@ public class GUI_MainLayout extends JFrame {
         setLocationRelativeTo(null);
         setLayout(new BorderLayout(0, 0));
         setUndecorated(true);
-
+        
         // ================================ Title Bar ================================
         tittleBar = new GUI_TittleBar(this);
 
@@ -42,7 +42,7 @@ public class GUI_MainLayout extends JFrame {
         Sidebar.productPanel = new JPanel();
         Sidebar.productPanel.setBackground(Color.GREEN);
         Sidebar.productPanel.add(new JLabel("Danh sách sản phẩm"));
-
+        
         Sidebar.productPanel = new GUI_Product();
 
         Sidebar.orderPanel = new GUI_SaleInvoice(logined);
@@ -123,5 +123,5 @@ public class GUI_MainLayout extends JFrame {
         add(Sidebar, BorderLayout.WEST);
         add(contentPanel, BorderLayout.CENTER);
     }
-
+    
 }
